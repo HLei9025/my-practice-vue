8982ac6ed29d39fb357e84ce340b0e7bd89b5e4d
HLei9025 <907978432@qq.com>
2020-08-13 20:55:21 +0800

    添加动态页面title+添加页面小图标

--- src/router/index.js
+++ src/router/index.js
@@ -11,31 +11,49 @@ const routes = [
     {
         path: '/test1',
         name: '1',
-        component: ()=>import('../pages/test1/Test1')
+        component: ()=>import('../pages/test1/Test1'),
+        meta: {
+            title: 'test1'
+        }
     },
     {
         path: '/test2',
         name: '2',
-        component: ()=>import('../pages/test2/Test2')
+        component: ()=>import('../pages/test2/Test2'),
+        meta: {
+            title: 'test2'
+        }
     },
     {
         path: '/test3',
         name: '3',
-        component: ()=>import('../pages/test3/Test3')
+        component: ()=>import('../pages/test3/Test3'),
+        meta: {
+            title: 'test3'
+        }
     },
     {
         path: '/test4',
         name: '4',
-        component: ()=>import('../pages/test4/Test4')
+        component: ()=>import('../pages/test4/Test4'),
+        meta: {
+            title: 'test4'
+        }
     },
     {
         path: '/test5',
         name: '5',
-        component: ()=>import('../pages/test5/Test5')
+        component: ()=>import('../pages/test5/Test5'),
+        meta: {
+            title: 'test5'
+        }
     },
     {
         path: '/404',
-        component: ()=>import('../components/NotFind')
+        component: ()=>import('../components/NotFind'),
+        meta: {
+            title: '404'
+        }
     },
     {
         path: '*',
@@ -46,6 +64,11 @@ const routes = [
 const router = new VueRouter({
     mode: 'history',
     routes
+})
+
+router.beforeEach((to, from, next)=>{
+    document.title = to.meta.title
+    next()
 })
 
 export default router;
---
+++ vue.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  pwa: {
+    iconPaths: {
+      favicon32: 'test.png',
+      favicon16: 'test.png',
+      appleTouchIcon: 'test.png',
+      maskIcon: 'test.png',
+      msTileImage: 'test.png'
+    }
+  }
+}
